db749fe5e3a33ce4dcc4f33b026b7bd9dbb4579e
DennisFan <dennis515jd@gmail.com>
2026-05-05 00:35:42 +0800

    BaseTileBlock

---
+++ GameSystem/WorldSystem/Tiles/BaseTileBlock.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+class_name BaseTileBlock
+
+## 所有世界瓷磚的基底類別
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var damageable: Damageable = $Damageable
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	# 統一設定碰撞層 (Layer 8: Wall)
+	collision_layer = BitmaskManager.LAYER_WALL
+	collision_mask = 0
+	
+	if damageable:
+		if not damageable.destroyed.is_connected(_on_destroyed):
+			damageable.destroyed.connect(_on_destroyed)
+
+## 當被摧毀時的預設行為
+func _on_destroyed() -> void:
+	queue_free()
+
+## 支援外部呼叫以相容舊邏輯
+func damage(amount: float, source_team_id: int = -1) -> void:
+	if damageable:
+		damageable.take_damage(amount, source_team_id)
--- GameSystem/WorldSystem/Tiles/Stone.gd
+++ GameSystem/WorldSystem/Tiles/Stone.gd
@@ -1,16 +1,4 @@
-extends StaticBody2D
+extends BaseTileBlock
 class_name StoneTile
 
-## 64x64 岩石瓷磚，使用標準 Damageable 組件
-
-@onready var damageable: Damageable = $Damageable
-
-func _ready() -> void:
-	collision_layer = BitmaskManager.LAYER_WALL
-	collision_mask = 0
-	
-	if not damageable.destroyed.is_connected(queue_free):
-		damageable.destroyed.connect(queue_free)
-
-func damage(amount: float, source_team_id: int = -1) -> void:
-	damageable.take_damage(amount, source_team_id)
+## 64x64 岩石瓷磚
